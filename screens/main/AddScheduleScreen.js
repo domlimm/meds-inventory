07337dc19804473addcf389782704ec7724e1443
Dominic Lim <dominiclimkj3448@gmail.com>
2020-05-14 18:06:06 +0800

    - Converted Select to List for last intake in add schedule screen - Added Loading indicator to selected last intake timing button, but unable to see spinner

--- screens/main/AddScheduleScreen.js
+++ screens/main/AddScheduleScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import { StyleSheet, ScrollView, KeyboardAvoidingView, Dimensions, Platform } from 'react-native';
 import {
   Layout,
@@ -14,27 +14,36 @@ import {
   IndexPath,
   Select,
   SelectItem,
-  Button
+  Button,
+  List,
+  ListItem
 } from '@ui-kitten/components';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+import LoadingIndicator from '../../components/LoadingIndicator';
 
 const { height, width } = Dimensions.get('window');
 
 const AddScheduleScreen = props => {
+  // Main States
   const [showInfo, setShowInfo] = useState(false);
   const [modeIndex, setModeIndex] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(false);
   const [freqIndex, setFreqIndex] = useState(new IndexPath(0));
 
+  // Daily selection States
   const [dailyIndex, setDailyIndex] = useState(new IndexPath(0));
+  const [dailyStartTime, setDailyStartTime] = useState(null);
 
+  // Hourly selection States
   const [hourIndex, setHourIndex] = useState(new IndexPath(0));
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
   const [hasTime, setHasTime] = useState(false);
-  const [time, setTime] = useState(null);
-  const [lastIntake, setLastIntake] = useState(new IndexPath(0));
+  const [hourlyStartTime, setHourlyStartTime] = useState(null);
+  const [hourlyLastIntake, setHourlyLastIntake] = useState(new IndexPath(0));
   const [triggered, setTriggered] = useState(false);
   const [lastIntakeArr, setLastIntakeArr] = useState([]);
+  const [lastIntakePos, setLastIntakePos] = useState(null);
 
   const CloseIcon = props => <Icon {...props} name='close-outline' />;
 
@@ -108,21 +117,22 @@ const AddScheduleScreen = props => {
     'Alternate Days',
     'Day/s of Week'
   ];
-  const intakeDaily = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  const intakeDaily = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'Custom'];
   const intakeHourly = ['Select Interval', '0.5', '1', '2', '3', '4', '6', '8', '12'];
 
   const selected_freq_index = freqIndex.row;
   const freqDisplay = freq[selected_freq_index];
 
-  const renderFreqItems = data => <SelectItem title={data} key={data} />;
-  const renderIntakeItems = data => <SelectItem title={data} key={data} />;
-  const renderHourSpecificItems = data => {
-    if (data.trim() === button_time.trim()) {
-      return <SelectItem key={data} disabled={true} title={`${data} - START TIME`} />;
-    } else {
-      return <SelectItem key={data} title={data} />;
+  const renderFreqItems = data => {
+    if (data === 'Alternate Days') {
+      return <SelectItem disabled={true} title={data} key={data} />;
     }
+    return <SelectItem title={data} key={data} />;
   };
+  const renderIntakeItems = data => <SelectItem title={data} key={data} />;
+
+  const dailyPos = dailyIndex.row;
+
   const Daily_Element = () => (
     <Layout style={styles.inputContainer}>
       <Layout style={styles.inputContainer}>
@@ -130,14 +140,17 @@ const AddScheduleScreen = props => {
           label={<InputLabel title='No. of Intake/s Daily' />}
           onSelect={index => setDailyIndex(index)}
           selectedIndex={dailyIndex}
-          value={intakeDaily[dailyIndex.row]}
+          value={intakeDaily[dailyPos]}
         >
           {intakeDaily.map(renderIntakeItems)}
         </Select>
       </Layout>
-      <Layout style={styles.inputContainer}>
-        <Button onPress={() => console.log('Add time elements')}>ADD REMINDER (counter)</Button>
-      </Layout>
+
+      {dailyPos === intakeDaily.length - 1 ? (
+        <Layout style={styles.inputContainer}>
+          <Button onPress={() => console.log('Add time elements')}>ADD REMINDER (counter)</Button>
+        </Layout>
+      ) : null}
     </Layout>
   );
 
@@ -145,21 +158,22 @@ const AddScheduleScreen = props => {
     setTimePickerVisibility(!isTimePickerVisible);
   };
 
-  let button_hour = new Date(time).getHours();
+  let button_hour = new Date(hourlyStartTime).getHours();
   button_hour = button_hour % 12;
   button_hour = button_hour ? button_hour : 12;
 
-  let button_minutes = new Date(time).getMinutes();
+  let button_minutes = new Date(hourlyStartTime).getMinutes();
   button_minutes = button_minutes < 10 ? '0' + button_minutes : button_minutes;
 
   const button_time = `${button_hour}:${button_minutes} ${
-    new Date(time).getHours() >= 12 ? 'PM' : 'AM'
+    new Date(hourlyStartTime).getHours() >= 12 ? 'PM' : 'AM'
   }`;
 
   const onTimeChangeHandler = selected_time => {
     setTimePickerVisibility(Platform.OS === 'ios');
+    setIsLoading(true);
     setHasTime(true);
-    setTime(selected_time);
+    setHourlyStartTime(selected_time);
     setTriggered(true);
   };
 
@@ -167,7 +181,7 @@ const AddScheduleScreen = props => {
 
   const generate_lastintake = () => {
     let tempArr = [];
-    let first_intake = new Date(time);
+    let first_intake = new Date(hourlyStartTime);
     let first_hour = first_intake.getHours();
     let first_minute = first_intake.getMinutes();
     let first_intake_minutes = first_hour * 60 + first_minute;
@@ -178,10 +192,7 @@ const AddScheduleScreen = props => {
     const total_daily_cycle = day_tot_minutes + first_intake_minutes;
     setLastIntakeArr([]);
 
-    console.log('--new--');
-
     for (let i = 0; first_intake_minutes < total_daily_cycle; i++) {
-      console.log('[start] first_intake_minutes', first_intake_minutes);
       let hourA = Math.floor(first_intake_minutes / 60);
       let hour = hourA >= 24 ? hourA - 24 : hourA;
       let minute = first_intake_minutes % 60;
@@ -191,26 +202,47 @@ const AddScheduleScreen = props => {
       ).slice(-2)} ${meridiem[hour < 12 ? 0 : 1]}`;
 
       first_intake_minutes += final_interval;
-
-      console.log('hour', hour);
-      console.log('minute', minute);
-      console.log('i', i);
-      console.log('tempArr[i]', tempArr[i]);
-      console.log('[end] first_intake_minutes', first_intake_minutes);
-      console.log('==================================================');
     }
-
     // slice array later based on last intake, then each of the timing save into db as array.
     // Bug : any timing doesnt go past 12am next day
     setLastIntakeArr(tempArr);
-    setLastIntake(new IndexPath(0));
+    setHourlyLastIntake(new IndexPath(0));
+  };
+
+  const [selectedLIntake, setSelectedLIntake] = useState(false);
+
+  const renderHourSpecificItems = ({ item, index }) => {
+    if (item.trim() === button_time.trim()) {
+      return (
+        <ListItem
+          key={index}
+          title={() => <InputLabel title={`${item} - START TIME`} style={{ fontWeight: 'bold' }} />}
+          disabled={true}
+        />
+      );
+    } else {
+      return (
+        <ListItem
+          key={index}
+          style={[selectedLIntake === index && { backgroundColor: '#4BB09E', borderRadius: 3 }]}
+          title={() => (
+            <InputLabel title={item} style={[selectedLIntake === index && { fontWeight: '700' }]} />
+          )}
+          onPress={() => {
+            setSelectedLIntake(index);
+            setLastIntakePos(index);
+          }}
+        />
+      );
+    }
   };
 
   useEffect(() => {
     if (triggered) {
       generate_lastintake();
+      setIsLoading(false);
     }
-  }, [hourly_index, time, triggered]);
+  }, [hourly_index, hourlyStartTime, triggered]);
 
   const Hours_Element = () => (
     <Layout>
@@ -227,8 +259,11 @@ const AddScheduleScreen = props => {
         </Select>
       </Layout>
       <Layout style={styles.inputContainer}>
-        <Button onPress={showTimeHandler} accessoryRight={ClockIcon}>
-          {hasTime ? `TIME SELECTED: ${button_time}` : 'SET START TIME'}
+        <Button
+          onPress={showTimeHandler}
+          accessoryRight={!isLoading ? ClockIcon : () => <LoadingIndicator />}
+        >
+          {isLoading ? 'LOADING' : hasTime ? `TIME SELECTED: ${button_time}` : 'SET START TIME'}
         </Button>
         <DateTimePickerModal
           mode='time'
@@ -237,8 +272,34 @@ const AddScheduleScreen = props => {
           onCancel={showTimeHandler}
           date={new Date(new Date().setHours(0, 0, 0, 0))}
         />
+        {hourlyStartTime ? <LastIntakeView loading={isLoading} /> : null}
       </Layout>
     </Layout>
+  );
+
+  const LastIntakeView = memo(
+    props => {
+      return (
+        <Layout style={styles.inputContainer}>
+          <InputLabel
+            title={
+              selectedLIntake ? `Last Intake: ${lastIntakeArr[selectedLIntake]}` : 'Set Last Intake'
+            }
+            style={{ marginVertical: 8 }}
+          />
+          <List
+            data={lastIntakeArr}
+            ItemSeparatorComponent={Divider}
+            renderItem={renderHourSpecificItems}
+            keyExtractor={item => item}
+            horizontal={true}
+            selectedProp={selectedLIntake}
+            style={{ paddingVertical: 4 }}
+          />
+        </Layout>
+      );
+    },
+    (prevProps, nextProps) => prevProps.loading === nextProps.loading
   );
 
   return (
@@ -282,18 +343,6 @@ const AddScheduleScreen = props => {
                 ) : selected_freq_index === 1 ? (
                   <Layout style={styles.inputContainer}>
                     <Hours_Element />
-                    {time ? (
-                      <Layout style={styles.inputContainer}>
-                        <Select
-                          label={<InputLabel title='Set Last Intake' />}
-                          onSelect={index => setLastIntake(index)}
-                          selectedIndex={lastIntake}
-                          value={lastIntakeArr[lastIntake.row]}
-                        >
-                          {lastIntakeArr.map(renderHourSpecificItems)}
-                        </Select>
-                      </Layout>
-                    ) : null}
                   </Layout>
                 ) : selected_freq_index === 2 ? null : selected_freq_index ===
                   3 ? null : selected_freq_index === 4 ? null : selected_freq_index ===
